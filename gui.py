09db7e9d3de9c63301f640f5962760ce08cf154f
Kozmik <koz.moore@gmail.com>
2020-08-10 20:10:47 -0700

    Tried to make a scrolling Frame. Trying another tack.

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
-from tkinter import StringVar, Toplevel
-from tkinter.ttk import Frame, Button, Entry, Label
+from tkinter import StringVar, Toplevel, Canvas
+from tkinter.ttk import Frame, Button, Entry, Label, Scrollbar
 from typing import List
 
 from base_widgets import JournalWidget
@@ -95,24 +95,40 @@ class TagsPopup(Toplevel):
         for i in range(6):
             self.grid_columnconfigure(index=i, weight=1)
         self.protocol('WM_DELETE_WINDOW', self.exit)
+
         self._parent = parent
+        self._selected_tags = selected
+        self._selected_buttons: List[TagButton] = []
+        self._unselected_tags = unselected
+        self._unselected_buttons: List[TagButton] = []
 
         self._filter_var = StringVar()
         self._trace = self._filter_var.trace_add('write', self.regrid)
 
         self._selected_label = Label(master=self, text='In')
         self._unselected_label = Label(master=self, text='Out')
-        self._selected_frame = Frame(master=self)
-        self._unselected_frame = Frame(master=self)
+
+        unselected_frame = Frame(master=self, height=232)
+        selected_frame = Frame(master=self, height=232)
+        unselected_frame.grid_columnconfigure(index=0, weight=1)
+        selected_frame.grid_columnconfigure(index=0, weight=1)
+        unselected_frame.grid_propagate(False)
+        selected_frame.grid_propagate(False)
+
+        self._unselected_canvas = Canvas(master=unselected_frame)
+        self._selected_canvas = Canvas(master=selected_frame)
+        self._unselected_canvas.grid_columnconfigure(index=0, weight=1)
+        self._selected_canvas.grid_columnconfigure(index=0, weight=1)
+
+        unselected_scrollbar = Scrollbar(master=unselected_frame, command=self._unselected_canvas.yview)
+        selected_scrollbar = Scrollbar(master=selected_frame, command=self._selected_canvas.yview)
+        self._unselected_canvas.configure(yscrollcommand=unselected_scrollbar.set)
+        self._selected_canvas.configure(yscrollcommand=selected_scrollbar.set)
+
         self._none = Button(master=self, text='None', command=self.select_none)
         self._invert = Button(master=self, text='Invert', command=self.select_invert)
         self._all = Button(master=self, text='All', command=self.select_all)
         self._filter = Entry(master=self, textvariable=self._filter_var)
-
-        self._selected_tags = selected
-        self._selected_buttons: List[TagButton] = []
-        self._unselected_tags = unselected
-        self._unselected_buttons: List[TagButton] = []
 
         self._none.grid(row=0, column=0, columnspan=2, sticky='ew')
         self._invert.grid(row=0, column=2, columnspan=2, sticky='ew')
@@ -120,6 +136,18 @@ class TagsPopup(Toplevel):
         self._filter.grid(row=1, columnspan=6, sticky='ew')
         self._unselected_label.grid(row=2, column=0, columnspan=3)
         self._selected_label.grid(row=2, column=3, columnspan=3)
+        self._unselected_canvas.grid(row=0, column=0, sticky='ew')
+        self._selected_canvas.grid(row=0, column=0, sticky='ew')
+        unselected_scrollbar.grid(row=0, column=1, sticky='ns')
+        selected_scrollbar.grid(row=0, column=1, sticky='ns')
+        Frame(master=self._unselected_canvas).grid()    # Dummy frame
+        Frame(master=self._selected_canvas).grid()      # Dummy frame
+        unselected_frame.grid(row=3, column=0, columnspan=3, sticky='nwe')
+        selected_frame.grid(row=3, column=3, columnspan=3, sticky='nwe')
+
+        # height = sum([self.grid_slaves(row=x, column=0)[0].winfo_height() for x in (0, 1, 2)])
+        # print(height)
+
         self.regrid()
 
     @property
@@ -164,20 +192,20 @@ class TagsPopup(Toplevel):
         self.regrid()
 
     def regrid(self, *args):
-        unselected = Frame(master=self)
-        selected = Frame(master=self)
+        unselected_frame = Frame(master=self._unselected_canvas, height=232)
+        selected_frame = Frame(master=self._selected_canvas, height=232)
         self._unselected_buttons.clear()
         self._selected_buttons.clear()
         self._selected_tags.sort()
         self._unselected_tags.sort()
         for tag in self._unselected_tags:
             if self._filter_var.get().lower() in tag.lower():
-                button = TagButton(tag=tag, text=tag, master=unselected, takefocus=True)
+                button = TagButton(tag=tag, text=tag, master=unselected_frame, takefocus=True)
                 button.configure(command=lambda b=button: self.swap(b))
                 self._unselected_buttons.append(button)
         for tag in self._selected_tags:
             if self._filter_var.get().lower() in tag.lower():
-                button = TagButton(tag=tag, text=tag, master=selected, takefocus=True)
+                button = TagButton(tag=tag, text=tag, master=selected_frame, takefocus=True)
                 button.configure(command=lambda b=button: self.swap(b))
                 self._selected_buttons.append(button)
 
@@ -187,17 +215,18 @@ class TagsPopup(Toplevel):
         for i in range(len(self._selected_buttons)):
             self._selected_buttons[i].grid(row=i, sticky='ew')
 
-        selected.columnconfigure(index=0, weight=1)
-        unselected.columnconfigure(index=0, weight=1)
+        selected_frame.grid_columnconfigure(index=0, weight=1)
+        unselected_frame.grid_columnconfigure(index=0, weight=1)
 
-        temp1 = self._selected_frame
-        temp2 = self._unselected_frame
-        self._selected_frame = selected
-        self._unselected_frame = unselected
-        self._unselected_frame.grid(row=3, column=0, columnspan=3, sticky='ewn')
-        self._selected_frame.grid(row=3, column=3, columnspan=3, sticky='ewn')
+        temp1 = self._selected_canvas.grid_slaves()[0]
+        temp2 = self._unselected_canvas.grid_slaves()[0]
+        unselected_frame.grid(sticky='ewn')
+        selected_frame.grid(sticky='ewn')
+
         temp1.destroy()
         temp2.destroy()
+        self._unselected_canvas.config(scrollregion=self._unselected_canvas.bbox("all"))
+        self._selected_canvas.config(scrollregion=self._selected_canvas.bbox("all"))
 
     def exit(self):
         self._selected_tags.sort()
@@ -213,7 +242,7 @@ def _test():
     root.grid_columnconfigure(index=0, weight=1)
     tags = TagsFrame(master=root, reader=reader)
     tags.pack()
-    tags.selected_tags = ['Purple', 'Green', 'Blue']
+    tags.selected_tags = ['Purple', 'Green', 'Blue', '1', '2', '3']
     root.mainloop()
 
 
